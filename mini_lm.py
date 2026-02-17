70075e2552f9caa5c1642e11863683335d0ad0cb
anuththiga <anuththiga25jt@gmail.com>
2026-02-17 11:08:33 +0800

    counting biograms in a 2D torch tensor

--- mini_lm.py
+++ mini_lm.py
@@ -23,3 +23,20 @@ for w in words:
 
 # sorting the key value
 sorted(c.items(), key = lambda kv: -kv[1])
+
+# counting bigrams in a 2D torch tensor
+import torch
+N = torch.zeros((28,28), dtype=torch.int32)
+
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i for i,s in enumerate(chars)}
+stoi['<S>'] = 26
+stoi['<E>'] = 27
+
+for w in words:
+  chs = ['<S>'] + list(w) + ['<E>']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    N[ix1, ix2] += 1
+
